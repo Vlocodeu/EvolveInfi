5e17e5a5fa86958948b2b56aea3701407d96e695
Vlocodeu <ourvlocodeu@gmail.com>
2025-03-07 14:37:50 +0530

    make all set

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true, // Optional: Ensures clean URLs
+  reactStrictMode: true,
+  experimental: {
+    appDir: true, // Ensure App Router is enabled
+  },
 };
 
 export default nextConfig;
